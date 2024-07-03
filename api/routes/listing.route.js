592345588572e2f9e9fea5e4343c2be0ff700c8c
GautamUIET <gautamjain09687@gmail.com>
2024-07-03 19:05:56 +0530

    listining serach api router

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListening ,deleteListening,updateListing,getListing} from '../controllers/lisning.controller.js';
+import { createListening ,deleteListening,updateListing,getListing,getListings} from '../controllers/lisning.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,createListening);
 router.delete('/delete/:id',verifyToken,deleteListening);
 router.post('/update/:id', verifyToken, updateListing);
-router.get('/get/:id',verifyToken, getListing);
+router.get('/get/:id',getListing);
+router.get('/get', getListings);
 export default router;
