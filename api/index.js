98037b9ff7e39eb71c4fffab976852c9f25e29d6
GautamUIET <gautamjain09687@gmail.com>
2024-07-04 20:42:43 +0530

    last commit

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
 import listeningRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 const app= express();
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO)
    console.log(err);
 })
 
+const __dirname = path.resolve();
+
 app.listen(3000,()=>{
     console.log('Server is Running on Port 3000');
 })
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/user",userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listeningRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 app.use((err,req,res,next) =>{
    const statusCode = err.statusCode || 500;
    const message = err.message || 'Internal Server Error';
